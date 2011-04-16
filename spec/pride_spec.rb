ab767d9b5f0507f59b0755177a029d61313e772d
Mark Rada <mrada@marketcircle.com>
2011-04-16 02:33:43 -0400

    Pending specs!

---
+++ spec/pride_spec.rb
@@ -0,0 +1,18 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rspec/pride'
+
+describe 'passing' do
+
+  it 'should print a star for passing tests'
+  it 'should cycle colours for the passing tests'
+  it 'should print a red F for failed tests'
+  it 'should print a black P for pending tests'
+  it 'should print a read F for error tests'
+
+  describe 'the summary' do
+    it 'should print the number of passing tests'
+    it 'should print the number of failing tests'
+    it 'should print the number of pending tests'
+  end
+
+end
